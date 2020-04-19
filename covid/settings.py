e84443f732f339b215093ba7688831fd821b152e
bran31 <bran31@gmail.com>
2020-04-19 06:15:55 -0400

    trying to deploy 2

--- covid/settings.py
+++ covid/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['station43.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -160,5 +160,5 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Configure Django app for Heroku
-import django_heroku
+
 django_heroku.settings(locals())
